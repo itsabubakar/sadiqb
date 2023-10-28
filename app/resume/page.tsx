2a1d1a95142cde5562e269d8215efaf4410991fb
Sadiq <seeker99157@gmail.com>
2023-10-28 12:59:23 +0100

    escaped character

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -14,7 +14,7 @@ const page = (props: Props) => {
                 experience
                 skills
             */}
-                <p className="font-body text-lg">Hello! I'm Sadiq Bilyamin, and I'm thrilled to share a bit about myself with you. For the past two years, I've been exploring the ins and outs of React, and recently, I've added PHP to my toolkit, opening up new possibilities in my coding adventures. Diving into the world of databases, I'm comfortable dancing between SQL and NoSQL, ensuring a well-rounded approach to handling data.</p>
+                <p className="font-body text-lg">Hello! I&apos;m Sadiq Bilyamin, and I&apos;m thrilled to share a bit about myself with you. For the past two years, I&apos;ve been exploring the ins and outs of React, and recently, I&apos;ve added PHP to my toolkit, opening up new possibilities in my coding adventures. Diving into the world of databases, I&apos;m comfortable dancing between SQL and NoSQL, ensuring a well-rounded approach to handling data.</p>
                 <p className="font-body text-lg pt-4">When it comes to the mobile scene, you can find me in the groove of React Native, shaping user experiences that are both smooth and exciting. I love to read and enjoy getting lost in a good book.</p>
                 <div className="py-2">
                     <ResumeLink />
